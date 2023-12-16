bd35d70032005ffc97c3a4e06abae5bb143fa632
Ralph Bacon <20911308+RalphBacon@users.noreply.github.com>
2023-12-16 10:54:58 +0000

    Add files via upload

---
+++ Code Examples/references.cpp
@@ -0,0 +1,31 @@
+#include <conio.h>
+#include <iostream>
+// Other includes not required here
+// #include <memory>
+// #include <string>
+// #include <vector>
+
+// Simple function to swap the two variables' values
+void swap(int& aa, int& bb)
+{
+    int temp = aa;
+    aa = bb;
+    bb = temp;
+    std::cout << "Inside 'swap' function: aa=" << aa << ", bb=" << bb << std::endl;
+    // Serial.println("Inside 'swap'...");
+    // printf("Inside 'swap' function: aa=%d, bb=%d", aa, bb);
+}
+
+// Standard "main" function, like the Arduino loop, except this one does not loop
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+	// Replace with Serial.print if this doesn not compile in your environment
+    std::cout << "BEFORE swap: Var a=" << a << ", var b=" << b << std::endl;
+    swap(a, b);
+    std::cout << "AFTER swap: Var a=" << a << ", var b=" << b << std::endl;
+
+    return 0;
+}
